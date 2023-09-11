49a218efbeb61419792afcdd8883398a76af0f4f
Denis Miagkov <tomich-84@mail.ru>
2023-09-11 23:58:45 +0300

    trigger on xp added

--- triggers.sql
+++ triggers.sql
@@ -97,6 +97,45 @@ DROP TRIGGER IF EXISTS p2p_successful_check ON verter;
 CREATE TRIGGER p2p_successful_check BEFORE INSERT OR UPDATE ON verter
 FOR EACH ROW EXECUTE PROCEDURE is_successful_check();
 
+--
+CREATE OR REPLACE FUNCTION check_maximum_for_xp(xp_amount int, _check int) RETURNS bool AS $$
+BEGIN 
+	IF (xp_amount > (SELECT MaxXP 
+						FROM tasks t JOIN checks c ON t.title = c.task
+						WHERE c.id = _check)) THEN 
+		RETURN FALSE;
+	ELSE 
+		RETURN TRUE;
+	END IF;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION check_status_for_xp(_check int) RETURNS bool AS $$
+BEGIN 
+	IF (((SELECT state FROM p2p WHERE "Check" = _check ORDER BY id DESC LIMIT 1) = 'Success') 
+		AND (SELECT state FROM verter WHERE "Check" = _check ORDER BY id DESC LIMIT 1) 
+		IN (NULL, 'Success')) THEN 
+		RETURN TRUE;
+	ELSE
+		RETURN FALSE;
+	END IF;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION check_xp() RETURNS TRIGGER AS $$
+BEGIN 
+	IF (check_maximum_for_xp(NEW.XPAmount, NEW."Check") = FALSE) THEN 
+		RAISE EXCEPTION 'XP amount can not be more then max XP of the task';
+	ELSEIF (check_status_for_xp(NEW."Check") = FALSE ) THEN 
+		RAISE EXCEPTION 'XP amount can not be received for not successful task';
+	END IF;
+	RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS is_xp_correct ON XP;
+CREATE TRIGGER is_xp_correct BEFORE INSERT OR UPDATE ON XP
+FOR EACH ROW EXECUTE PROCEDURE check_xp();
 
 
 
