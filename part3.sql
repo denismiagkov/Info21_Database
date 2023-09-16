3ee685a616ab0316e967678db4efbc7b25d64d56
Denis Miagkov <tomich-84@mail.ru>
2023-09-17 00:02:13 +0300

    functions get_number_of_transferred_peerpoints and get_most_frequently_checked_task added

--- part3.sql
+++ part3.sql
@@ -23,7 +23,7 @@
 --$$ LANGUAGE plpgsql;
 
 SELECT * FROM present_transferred_points();
-
+--2
 CREATE OR REPLACE FUNCTION xp_by_peer() RETURNS TABLE 
 (Peer varchar, Task varchar, XP integer) AS $$
 BEGIN
@@ -38,7 +38,7 @@ END
 $$ LANGUAGE plpgsql;
 
 --SELECT * FROM xp_by_peer();
-
+--3
 CREATE OR REPLACE FUNCTION get_not_exited_peers(dt date) RETURNS SETOF varchar AS $$
 BEGIN 
 	RETURN query
@@ -51,5 +51,49 @@ END
 $$ LANGUAGE plpgsql;
 
 --SELECT * FROM get_not_exited_peers('2023-09-15');
+--4
+CREATE OR REPLACE FUNCTION get_number_of_transferred_peerpoints() RETURNS TABLE 
+(Peer varchar, PointsChange bigint) AS $$
+BEGIN
+	RETURN query
+	WITH tb AS (
+		SELECT CheckingPeer, PointsAmount
+		FROM transferredpoints t 
+		UNION ALL 
+		SELECT CheckedPeer, PointsAmount
+		FROM transferredpoints t )
+	SELECT CheckingPeer AS Peer, sum(pointsamount) AS PointsChange
+	FROM tb
+	GROUP BY Peer
+	ORDER BY PointsChange DESC ; 
+END
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM get_number_of_transferred_peerpoints();
+
+--5
+
+
+--6
+CREATE OR REPLACE FUNCTION  get_most_frequently_checked_task() RETURNS TABLE ("Day" date, Task varchar) AS $$
+BEGIN 
+	RETURN query
+	WITH max_count_by_day AS (
+		WITH all_tasks_count AS (
+			SELECT c.date, c.task, count(*)
+			FROM checks c 
+			GROUP BY c.task, c.date)
+		SELECT date, all_tasks_count.task, count,
+		max(count) OVER (PARTITION BY date) AS max
+		FROM all_tasks_count )
+	SELECT date AS DAY, max_count_by_day.task AS Task
+	FROM max_count_by_day
+	WHERE count = max;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM get_most_frequently_checked_task();
+
+
 
 
