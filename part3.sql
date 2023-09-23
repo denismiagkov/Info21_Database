f2f4ba1b464f6a71436ad0b4210179237ff50660
Denis Miagkov <tomich-84@mail.ru>
2023-09-23 22:40:56 +0300

    functions modified to procedures

--- part3.sql
+++ part3.sql
@@ -50,29 +50,29 @@ $$ LANGUAGE plpgsql;
 --SELECT * FROM get_not_exited_peers('2023-09-15');
 
 --3.4. Посчитать изменение в количестве пир поинтов каждого пира по таблице TransferredPoints
-CREATE OR REPLACE FUNCTION get_number_of_transferred_peerpoints() RETURNS TABLE 
-(Peer varchar, PointsChange bigint) AS $$
+CREATE OR REPLACE PROCEDURE get_number_of_transferred_peer_points(get_result refcursor) AS $$
 BEGIN
-	RETURN query
-	WITH tb AS (
+	OPEN get_result FOR EXECUTE 
+	'WITH tb AS (
 		SELECT CheckingPeer, PointsAmount
 		FROM transferredpoints t 
 		UNION ALL 
-		SELECT CheckedPeer, PointsAmount
+		SELECT CheckedPeer, PointsAmount*(-1)
 		FROM transferredpoints t )
 	SELECT CheckingPeer AS Peer, sum(pointsamount) AS PointsChange
 	FROM tb
 	GROUP BY Peer
-	ORDER BY PointsChange DESC ; 
+	ORDER BY PointsChange DESC' ; 
 END
 $$ LANGUAGE plpgsql;
---SELECT * FROM get_number_of_transferred_peerpoints();
+--CALL get_number_of_transferred_peer_points('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.5. Посчитать изменение в количестве пир поинтов каждого пира по таблице, возвращаемой первой функцией из Part 3
-CREATE OR REPLACE FUNCTION get_peer_points_changed(OUT peer varchar, OUT points_change bigint) 
-RETURNS SETOF record AS $$
+CREATE OR REPLACE PROCEDURE get_peer_points_changed(get_result refcursor) AS $$
 BEGIN 
-	RETURN query
+	OPEN get_result FOR 
 	SELECT COALESCE (t1.peer1, t2.peer2) peer, (COALESCE (sum1, 0) + COALESCE (sum2, 0)) points_change
 	FROM 
 		(SELECT peer1, sum(pointsamount) sum1
@@ -82,15 +82,18 @@ BEGIN
 		(SELECT peer2, sum(pointsamount)*(-1) sum2
 		FROM (SELECT * FROM present_transferred_points()) t
 		GROUP BY peer2) t2
-		ON t1.peer1 = t2.peer2;
+		ON t1.peer1 = t2.peer2
+	ORDER BY points_change DESC;
 END 
 $$ LANGUAGE plpgsql;
---SELECT * FROM get_peer_points_changed();
+--CALL get_peer_points_changed('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.6. Определить самое часто проверяемое задание за каждый день
-CREATE OR REPLACE FUNCTION  get_most_frequently_checked_task() RETURNS TABLE ("Day" date, Task varchar) AS $$
+CREATE OR REPLACE PROCEDURE  get_most_frequently_checked_task(get_result refcursor) AS $$
 BEGIN 
-	RETURN query
+	OPEN get_result FOR 
 	WITH max_count_by_day AS (
 		WITH all_tasks_count AS (
 			SELECT c.date, c.task, count(*)
@@ -104,13 +107,14 @@ BEGIN
 	WHERE count = max;
 END
 $$ LANGUAGE plpgsql;
---SELECT * FROM get_most_frequently_checked_task();
+--CALL get_most_frequently_checked_task('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.7. Найти всех пиров, выполнивших весь заданный блок задач и дату завершения последнего задания
-CREATE OR REPLACE FUNCTION get_peers_completed_block(block_name varchar, OUT peer varchar, OUT finish_day timestamp) 
-RETURNS SETOF record AS $$
+CREATE OR REPLACE PROCEDURE get_peers_completed_block(block_name varchar, get_result refcursor) AS $$
 BEGIN 
-	RETURN query 
+	OPEN get_result FOR   
 	WITH tbl AS (
 		SELECT  c.peer, c.task, pp.time AS finish_day,
 		DENSE_RANK()  OVER (PARTITION BY c.peer ORDER BY c.task) AS series
@@ -123,68 +127,52 @@ BEGIN
 	ORDER BY finish_day;
 END
 $$ LANGUAGE plpgsql;
---SELECT * FROM get_peers_completed_block ('SQL');
+--CALL get_peers_completed_block('linux', 'get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.8. Определить, к какому пиру стоит идти на проверку каждому обучающемуся
-CREATE OR REPLACE FUNCTION get_recommended_checking_peer() RETURNS SETOF record AS $$
-DECLARE 
-	Peer varchar;
-	RecommendedPeer varchar;
-	r record;
-BEGIN 
-	FOR peer IN (SELECT nickname AS Peer FROM peers)
-		LOOP 
-			WITH all_recommendations AS (
-				WITH peer_friends AS (
-					SELECT peer1
-					FROM friends f 
-					WHERE peer2=peer
-					UNION 
-					SELECT peer2 
-					FROM friends
-					WHERE peer1=peer)
-				SELECT pf.peer1, rec.recommendedpeer , 
-				count(rec.recommendedpeer) OVER (PARTITION BY rec.recommendedpeer) AS recommendations_number
-				FROM peer_friends pf JOIN recommendations rec ON pf.peer1=rec.peer)
-			SELECT DISTINCT ar.recommendedpeer INTO RecommendedPeer
-			FROM all_recommendations ar
-			WHERE ar.recommendedpeer != peer AND recommendations_number = (SELECT max(recommendations_number) 
-																			FROM all_recommendations);
-			r = row(Peer, RecommendedPeer);
-			RETURN NEXT r;
-		END LOOP;
-	RETURN;
-END
+CREATE OR REPLACE PROCEDURE get_recommended_checking_peer(get_result refcursor) AS $$
+BEGIN
+	OPEN get_result FOR
+	SELECT peer1 AS peer, recommendedpeer 
+	FROM (WITH 
+			fr AS 
+			(SELECT peer1, peer2 FROM friends
+			UNION 
+			SELECT peer2, peer1 FROM friends)
+		SELECT DISTINCT ON (peer1) peer1, recommendedpeer, count(recommendedpeer) max_count
+		FROM fr LEFT JOIN recommendations r ON fr.peer2 = r.peer
+		WHERE peer1 != recommendedpeer
+		GROUP BY peer1, recommendedpeer
+		ORDER BY peer1, max_count DESC) foo;
+END 
 $$ LANGUAGE plpgsql;
---SELECT * FROM  get_recommended_checking_peer() AS tbl(Peer varchar, RecommendedPeer varchar);
+--CALL get_recommended_checking_peer('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.9. Определить процент пиров, которые: *) Приступили только к блоку 1; 
 -- *) Приступили только к блоку 2; *) Приступили к обоим;  *) Не приступили ни к одному
-
-CREATE OR REPLACE FUNCTION  get_percent_of_peers_started_task(blockname1 varchar, blockname2 varchar, 
-OUT started_block_1 bigint, OUT started_block_2 bigint, OUT started_both_blocks bigint, 
-OUT didnt_start_any_block bigint) RETURNS SETOF record AS $$
-DECLARE 
-	block1 varchar = (blockname1 || 1);
-	block2 varchar = (blockname2 || 1);
-	total bigint = (SELECT (100 / (SELECT count(*) FROM peers)));
+CREATE OR REPLACE PROCEDURE  get_percent_of_peers_started_task(blockname1 varchar, blockname2 varchar, 
+get_result refcursor) AS $$
 BEGIN 
-	RETURN query 
+	OPEN get_result FOR 
 	WITH started_task AS (
 			SELECT peer , task, nickname,
 			dense_rank() over(PARTITION BY peer ORDER BY (task LIKE (blockname1 || '%'), 
 			task LIKE (blockname2 || '%'))) AS dr
 			FROM checks c FULL JOIN peers p ON c.peer = p.nickname),
-		sql_ AS (
-			SELECT count(*) sql_per 
+		block1 AS (
+			SELECT count(*) block1_per 
 			FROM (SELECT DISTINCT nickname FROM started_task
-			WHERE task LIKE (block1 || '%') AND (nickname NOT IN (SELECT DISTINCT nickname FROM started_task
-			WHERE task LIKE (block2 || '%'))))AS SQL),
-		linux AS (
-			SELECT count(*) linux_per 
+			WHERE task LIKE (blockname1 || '1') AND (nickname NOT IN (SELECT DISTINCT nickname FROM started_task
+			WHERE task LIKE (blockname2 || '1'))))AS block1),
+		block2 AS (
+			SELECT count(*) block2_per 
 			FROM (SELECT DISTINCT nickname FROM started_task
-			WHERE task LIKE (block2 || '%') AND (nickname NOT IN (SELECT DISTINCT nickname FROM started_task
-			WHERE task LIKE (block1 || '%')))) AS linux),
+			WHERE task LIKE (blockname2 || '1') AND (nickname NOT IN (SELECT DISTINCT nickname FROM started_task
+			WHERE task LIKE (blockname1 || '1')))) AS block2),
 		both_ AS (
 			SELECT count(*) both_per 
 			FROM (SELECT DISTINCT nickname FROM started_task
@@ -195,70 +183,84 @@ BEGIN
 			FROM started_task
 			WHERE task IS NULL)
 	SELECT 
-	(max(s.sql_per) * total) AS started_block_1, (max(l.linux_per) * total) AS started_block_1, 
-	(max(b.both_per) * total) AS started_both_blocks, (max(n.none_per) * total) AS didnt_start_any_block
-	FROM sql_ s 
-	FULL  JOIN linux l ON s.sql_per = l.linux_per 
-	FULL JOIN both_ b ON s.sql_per = b.both_per 
-	FULL JOIN none_ n ON s.sql_per = n.none_per;
+	(max(b1.block1_per) * (SELECT (100 / (SELECT count(*) FROM peers)))) AS started_block_1, 
+	(max(b2.block2_per) * (SELECT (100 / (SELECT count(*) FROM peers)))) AS started_block_2, 
+	(max(b.both_per) * (SELECT (100 / (SELECT count(*) FROM peers)))) AS started_both_blocks, 
+	(max(n.none_per) * (SELECT (100 / (SELECT count(*) FROM peers)))) AS didnt_start_any_block
+	FROM block1 b1 
+	FULL  JOIN block2 b2 ON b1.block1_per = b2.block2_per 
+	FULL JOIN both_ b ON b1.block1_per = b.both_per 
+	FULL JOIN none_ n ON b1.block1_per = n.none_per;
 END
 $$ LANGUAGE plpgsql;
---SELECT * FROM get_percent_of_peers_started_task('SQL', 'linux');
+--CALL get_percent_of_peers_started_task('SQL', 'linux', 'get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.10. Определить процент пиров, которые когда-либо успешно проходили проверку в свой день рождения
-CREATE OR REPLACE FUNCTION get_success_on_birthday(OUT SuccessfulChecks bigint,OUT UnsuccessfulChecks bigint) AS $$
-SELECT(
-(SELECT count(DISTINCT p.nickname) AS SuccessfulChecks
-FROM checks c 
-	FULL JOIN peers p ON c.peer = p.nickname 
-	FULL JOIN p2p pp ON c.id = pp."Check" 
-	FULL JOIN verter v ON c.id = v."Check" 
-WHERE ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM pp."time"))
+CREATE OR REPLACE PROCEDURE get_success_on_birthday(get_result refcursor) AS $$
+BEGIN 
+	OPEN get_result FOR 
+	SELECT (successfulchecks * 100 / (successfulchecks+unsuccessfulchecks)) AS successfulchecks,
+		(unsuccessfulchecks * 100 / (successfulchecks+unsuccessfulchecks)) AS unsuccessfulchecks
+	FROM
+	(SELECT * FROM 
+	(SELECT count(DISTINCT p.nickname) AS SuccessfulChecks
+	FROM checks c 
+		FULL JOIN peers p ON c.peer = p.nickname 
+		FULL JOIN p2p pp ON c.id = pp."Check" 
+		FULL JOIN verter v ON c.id = v."Check" 
+	WHERE ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM pp."time"))
 		 AND (EXTRACT (DAY FROM p.birthday) = EXTRACT(DAY FROM pp."time"))
 		 AND pp.state = 'Success' AND v.state IS NULL)
-	OR ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM v."time"))
+		OR ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM v."time"))
 		 AND (EXTRACT (DAY FROM p.birthday) = EXTRACT(DAY FROM v."time"))
-		 AND v.state = 'Success'))
-	* 100 / (SELECT count(*) FROM peers),
-(SELECT count(DISTINCT p.nickname) AS UnsuccessfulChecks
-FROM checks c 
-	FULL JOIN peers p ON c.peer = p.nickname 
-	FULL JOIN p2p pp ON c.id = pp."Check" 
-	FULL JOIN verter v ON c.id = v."Check" 
-WHERE ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM pp."time"))
+		 AND v.state = 'Success')) AS q1,
+	(SELECT count(DISTINCT p.nickname) AS UnsuccessfulChecks
+	FROM checks c 
+		FULL JOIN peers p ON c.peer = p.nickname 
+		FULL JOIN p2p pp ON c.id = pp."Check" 
+		FULL JOIN verter v ON c.id = v."Check" 
+	WHERE ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM pp."time"))
 		 AND (EXTRACT (DAY FROM p.birthday) = EXTRACT(DAY FROM pp."time"))
 		 AND pp.state = 'Failure' AND v.state IS NULL)
-	OR ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM v."time"))
+		OR ((EXTRACT (MONTH FROM p.birthday) = EXTRACT(MONTH FROM v."time"))
 		 AND (EXTRACT (DAY FROM p.birthday) = EXTRACT(DAY FROM v."time"))
-		 AND v.state = 'Failure'))
-	* 100 / (SELECT count(*) FROM peers));
-$$ LANGUAGE SQL;
---SELECT * FROM get_success_on_birthday();
+		 AND v.state = 'Failure')) AS q2) AS q;
+END 
+$$ LANGUAGE plpgsql;
+--CALL get_success_on_birthday('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.11. Определить всех пиров, которые сдали заданные задания 1 и 2, но не сдали задание 3
-CREATE OR REPLACE FUNCTION get_tasks_handed_over(task1 varchar, task2 varchar, task3 varchar) 
-RETURNS SETOF varchar AS $$
-		SELECT DISTINCT c.peer AS peers
-		FROM p2p pp JOIN checks c ON pp."Check" = c.id 
-		WHERE (c.task = task1 AND pp.state = 'Start')
-		INTERSECT 
-		SELECT DISTINCT c.peer
-		FROM p2p pp JOIN checks c ON pp."Check" = c.id 
-		WHERE (c.task = task2 AND pp.state = 'Start')
-		INTERSECT 
-		SELECT DISTINCT c.peer
-		FROM p2p pp JOIN checks c ON pp."Check" = c.id 
-		WHERE c.peer NOT IN (SELECT c.peer
-								FROM checks c JOIN p2p pp ON c.id = pp."Check" 
-								WHERE task = task3 AND pp.state = 'Start');
-$$ LANGUAGE SQL;
---SELECT * FROM get_tasks_handed_over('SQL1', 'SQL2', 'SQL3');
+CREATE OR REPLACE PROCEDURE get_tasks_handed_over(task1 varchar, task2 varchar, task3 varchar, 
+get_result refcursor) AS $$
+BEGIN 
+	OPEN get_result FOR
+	SELECT DISTINCT c.peer AS peers
+	FROM p2p pp JOIN checks c ON pp."Check" = c.id 
+	WHERE (c.task = task1 AND pp.state = 'Start')
+	INTERSECT 
+	SELECT DISTINCT c.peer
+	FROM p2p pp JOIN checks c ON pp."Check" = c.id 
+	WHERE (c.task = task2 AND pp.state = 'Start')
+	INTERSECT 
+	SELECT DISTINCT c.peer
+	FROM p2p pp JOIN checks c ON pp."Check" = c.id 
+	WHERE c.peer NOT IN (SELECT c.peer
+							FROM checks c JOIN p2p pp ON c.id = pp."Check" 
+							WHERE task = task3 AND pp.state = 'Start');
+END
+$$ LANGUAGE plpgsql;
+--CALL get_tasks_handed_over('SQL1', 'SQL2', 'SQL3', 'get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.12. Используя рекурсивное обобщенное табличное выражение, для каждой задачи вывести кол-во предшествующих ей задач
-CREATE OR REPLACE FUNCTION get_count_of_previous_tasks (OUT task varchar, OUT prev_count int) 
-RETURNS SETOF record AS $$
+CREATE OR REPLACE PROCEDURE  get_count_of_previous_tasks (get_result refcursor) AS $$
 BEGIN 
-	RETURN query
+	OPEN get_result FOR 
 	WITH RECURSIVE count_parent_task(task, prev_count) AS 
 	(
 		SELECT title, 0 FROM tasks WHERE parenttask IS NULL
@@ -271,24 +273,32 @@ BEGIN
 	ORDER BY task;
 END 
 $$ LANGUAGE plpgsql;
--- SELECT * FROM get_count_of_previous_tasks();
+--CALL get_count_of_previous_tasks('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.14. Определить пира с наибольшим количеством XP
-CREATE OR REPLACE FUNCTION get_peer_with_max_xp(OUT peer varchar, OUT xp bigint) AS $$
-WITH xp_by_peer AS (
-	SELECT peer, sum(xpamount) xp_sum 
-	FROM xp JOIN checks c ON xp."Check" = c.id 
-	GROUP BY peer)
-SELECT peer, xp_sum
-FROM xp_by_peer
-WHERE xp_sum = (SELECT max(xp_sum) FROM xp_by_peer);
-$$ LANGUAGE SQL;
--- SELECT * FROM get_peer_with_max_xp();
-
---3.15. Определить пиров, приходивших раньше заданного времени не менее N раз за всё время
-CREATE OR REPLACE FUNCTION get_peers_come_earlier(time_ time, n int) RETURNS SETOF varchar AS $$
+CREATE OR REPLACE PROCEDURE get_peer_with_max_xp(get_result refcursor) AS $$
 BEGIN 
-	RETURN query
+	OPEN get_result FOR 
+	WITH xp_by_peer AS (
+		SELECT peer, sum(xpamount) xp_sum 
+		FROM xp JOIN checks c ON xp."Check" = c.id 
+		GROUP BY peer)
+	SELECT peer, xp_sum
+	FROM xp_by_peer
+	WHERE xp_sum = (SELECT max(xp_sum) FROM xp_by_peer);
+END 
+$$ LANGUAGE plpgsql;
+--CALL get_peer_with_max_xp('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
+
+DROP FUNCTION get_peers_come_earlier;
+--3.15. Определить пиров, приходивших раньше заданного времени не менее N раз за всё время
+CREATE OR REPLACE PROCEDURE get_peers_come_earlier(time_ time, n int, get_result refcursor) AS $$
+BEGIN 
+	OPEN get_result FOR 
 	SELECT peer
 	FROM timetracking t 
 	WHERE t."time" < time_ AND state = 1
@@ -296,12 +306,14 @@ BEGIN
 	HAVING count(*) >=n;
 END
 $$ LANGUAGE plpgsql;
--- SELECT * FROM get_peers_come_earlier('09:00:00', 4);
+--CALL get_peers_come_earlier('09:00:00', 3, 'get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 -- 3.16. Определить пиров, выходивших за последние N дней из кампуса больше M раз
-CREATE OR REPLACE FUNCTION get_peers_got_out_more_than(period_ int, count_ int) RETURNS SETOF varchar AS $$
+CREATE OR REPLACE PROCEDURE get_peers_got_out_more_than(period_ int, count_ int, get_result refcursor) AS $$
 BEGIN 
-	RETURN query
+	OPEN get_result FOR
 	SELECT peer
 	FROM timetracking t 
 	WHERE state = 2  AND date BETWEEN (current_date - period_) AND now()
@@ -309,13 +321,14 @@ BEGIN
 	HAVING (count(state)-1) > count_;
 END 
 $$ LANGUAGE plpgSQL;
---SELECT * FROM get_peers_got_out_more_than(30, 1);
+--CALL get_peers_got_out_more_than(30, 2, 'get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 --3.17. Определить для каждого месяца процент ранних входов
-CREATE OR REPLACE FUNCTION get_early_entries(OUT MONTH text, OUT early_entries double precision) 
-RETURNS SETOF record AS $$
+CREATE OR REPLACE PROCEDURE get_early_entries(get_result refcursor) AS $$
 BEGIN
-	RETURN query
+	OPEN get_result FOR
 	WITH 
 	o_in AS (
 		SELECT to_char(date_trunc('month', t.date), 'Month') AS month, count(state) AS overall_in
@@ -332,8 +345,8 @@ BEGIN
 	FROM o_in LEFT JOIN e_in ON o_in.month = e_in.month;	
 END
 $$ LANGUAGE plpgsql;
---SELECT * FROM get_early_entries();
-
-
+--CALL get_early_entries('get_result');
+--FETCH ALL FROM "get_result";
+--CLOSE "get_result";
 
 
